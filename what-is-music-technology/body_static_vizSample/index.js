ef5edc2e620dd572c0dbbd42d6874746d03a54af
Balandino Di Donato <info@balandinodidonato.com>
2021-03-21 12:49:46 +0000

    adds border slide 1

--- what-is-music-technology/body_static_vizSample/index.js
+++ what-is-music-technology/body_static_vizSample/index.js
@@ -1,6 +1,6 @@
 let img;
 const colors = [[245, 66, 132], [255, 0, 0], [247, 95, 0], [247, 193, 0], [247, 247, 0], [181, 247, 0], [103, 247, 0], [0, 255, 0], [247, 45, 0], [178, 102, 255], [51, 255, 51], [255, 255, 255], [102, 178, 255], [51, 153, 255], [51, 51, 255], [255, 0, 127], [178, 102, 255], [255, 0, 255], [102, 0, 204], [0, 0, 255], [0, 0, 255], [51, 51, 255], [0, 255, 255], [0, 255, 255], [0, 255, 255]];
-const keypointsName = ["nose", "neck", "r_shoulder", "r_elbow", "r_wrist", "l_shoulder", "l_elbow", "l_wrist", "mid_hip", "r_hip", "r_knee", "r_ankle", "l_hip", "l_knee", "l_ankle", "r_eye", "l_eye", "l_ear", "r_ear", "r_big_toe", "r_small_toe", "r_heel", "l_big_toe", "r_small_toe", "r_heel"];
+const keypointsName = ["Nose", "Neck", "R Shoulder", "R Elbow", "R Wrist", "L shoulder", "L Elbow", "L Wrist", "Mid hip", "R Hip", "R Knee", "R Ankle", "L Hip", "L Knee", "L Ankle", "R Eye", "L Eye", "L Ear", "R Ear", "R Big toe", "R small_toe", "R Heel", "L Big toe", "R small_toe", "R Heel"];
 let imgBuffer, legendBuffer;
 
 function preload() {
@@ -9,16 +9,15 @@ function preload() {
 
 
 function setup() {
+  const ratio = img.width/img.height;
   const widthCanvas = windowWidth;
-  const heightCanvas = widthCanvas / 1.7777777777777777777777777778;
-  cnv = createCanvas(widthCanvas, heightCanvas);
-  cnv.position(0, 0);
-  tSize = width*0.019;
-
-  background(255);
+  const heightCanvas = widthCanvas / ratio;
+  let cnv = createCanvas(widthCanvas, heightCanvas);
   
-  imgBuffer = createGraphics(width*0.8, height*0.8);
-  legendBuffer = createGraphics(width*0.15, height);
+  
+  imgBuffer = createGraphics(width*0.7, height*0.7);
+  legendBuffer = createGraphics(width*0.14, height);
+  cnv.position((width-(legendBuffer.width+imgBuffer.width))/2);
 
   imgBuffer.image(img, 0, 0, imgBuffer.width, imgBuffer.height);
   
@@ -55,21 +54,22 @@ function showPointer(){
 }
 
 function drawLegend(){
-  const tSize = width*0.017;
+  const tSize = width*0.013;
   const tWidth = width*0.1;
   const boxX = 5+tWidth;
-  const tBoxWidth = tSize*0.9;
+  const tBoxWidth = tSize*0.8;
+  const tHeightMargin = legendBuffer.height * 0.027;
 
   for (let index = 0; index < keypointsName.length; index++) {
     // lables
     legendBuffer.fill(0);
     legendBuffer.noStroke();
     legendBuffer.textSize(tSize);
-    legendBuffer.text(keypointsName[index], 5, (index*tSize)+5, tWidth, tSize);
+    legendBuffer.text(keypointsName[index], 5, (index*tHeightMargin)+tSize, tWidth, tSize);
     //boxes
     legendBuffer.fill(colors[index][0], colors[index][1], colors[index][2]);
     legendBuffer.stroke(0);
     legendBuffer.strokeWeight(0.5);
-    legendBuffer.rect(boxX, (index*tSize)+5, tBoxWidth, tBoxWidth);
+    legendBuffer.rect(boxX, (index*tHeightMargin)+tSize, tBoxWidth, tBoxWidth);
   }
 }
